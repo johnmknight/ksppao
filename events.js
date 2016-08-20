9cecdc35d6c0ce12a95205904ce9c7201a39f436
John M Knight <johnmknight@users.noreply.github.com>
2016-08-20 19:51:50 -0400

    Add files via upload

---
+++ events.js
@@ -0,0 +1,108 @@
+
+var holidays = [];
+holidays.push(["January 1","New Year's Day", "We at the Kerbal Space Center wish you all on Earth a happy new years"]);
+holidays.push(["December 25","New Year's Day", "We at the Kerbal Space Center wish you all on Earth a Merry Christmas"]);
+holidays.push(["July 4","Independence Day", "We here at the Kerbal Space Program wish you all on Earth and in The United States, a Happy July 4th!"]);
+holidays.push(["July 29","John's Birthday","We wish our human translator a very happy birthday!"]);
+
+var spaceEvents = [];
+
+				   
+spaceEvents.push( ["January 1, 1958","Explorer 1 was the first satellite launched by the United States when it was sent into orbit on January 31, 1958. It was designed and built by the Jet Propulsion Laboratory (JPL) of the California Institute of Technology. The satellite was sent aloft from Cape Canaveral in Florida by the Jupiter C rocket that was designed, built, and launched by the Army Ballistic Missile Agency (ABMA) under the direction of Dr. Wernher Von Braun."]);
+spaceEvents.push( ["January 14, 2004","President Bush proposed a new space program that would send humans back to the moon by 2015 and establish a base to Mars and beyond."]);
+spaceEvents.push( ["January 14, 2008","The NASA space probe Messenger skimmed 124 miles above Mercury.			"]);
+spaceEvents.push( ["January 15, 2006","NASA spacecraft Stardust returned safely to Earth in a desert near Salt Lake City with the first dust ever collected from a comet.						"]);
+spaceEvents.push( ["January 24, 1986","Voyager 2 began transmitting images from Uranus.									"]);
+spaceEvents.push( ["January 27, 1967","Astronauts Gus Grissom, Ed White, and Roger Chaffee were killed in an accidental fire in a command module on the launch pad."]);
+spaceEvents.push( ["January 28, 1986","The Space Shuttle Challenger exploded seconds after liftoff. 					   "]);
+spaceEvents.push( ["February 1, 2003","The Space Shuttle Columbia broke up on re-entry into the Earth’s atmosphere."]);
+spaceEvents.push( ["February 2, 1995","Eileen Collins became the first female Shuttle pilot."]);
+spaceEvents.push( ["February 3, 1966","The Russian spacecraft Luna 9 became the first spacecraft to land on the moon. "]);
+spaceEvents.push( ["February 3, 1984","Astronaut Bruce McCandless became the first man to take an untethered space walk. "]);
+spaceEvents.push( ["February 3, 1994","Sergei Krikalev became the first Russian cosmonaut to fly on a Space Shuttle."]);
+spaceEvents.push( ["February 12, 2001","NEAR landed on the surface of Eros. "]);
+spaceEvents.push( ["February 13, 2003","An investigative panel found that superheated air almost certainly seeped through a breach in space shuttle Columbia’s left wing and possibly its wheel compartment during the craft’s fiery descent, resulting in the deaths of all seven astronauts."]);
+spaceEvents.push( ["February 14, 2000"," The U.S. Near Earth Asteroid Rendezvous (NEAR) spacecraft began transmitting images of the asteroid Eros."]);
+spaceEvents.push( ["February 20, 1962","Astronaut John Glenn became the first American in orbit. "]);
+spaceEvents.push( ["February 20, 1986","The core section of the Space Station Mir was launched."]);
+spaceEvents.push( ["March 6, 2009","The NASA spacecraft Kepler was launched. Its mission is to search for planets outside our solar system, in a distant area of the Milky Way.						"]);
+spaceEvents.push( ["March 18, 1965","While tethered to his spacecraft, cosmonaut Alexi Leonov became the first man to walk in space. 					   "]);
+spaceEvents.push( ["April 4, 1983","The second Space Shuttle, Challenger, was launched. "]);
+spaceEvents.push( ["April 11, 1970","Apollo 13 was launched. "]);
+spaceEvents.push( ["April 12, 1961","Russian cosmonaut Yuri Gagarin became the first human in space."]);
+spaceEvents.push( ["April 12, 1981","Columbia became the first Space Shuttle to be launched. "]);
+spaceEvents.push( ["April 19, 1971","The Soviet space station Salyut 1 was launched."]);
+spaceEvents.push( ["April 24, 1967","Cosmonaut Vladimir M. Komarov was killed in a crash when the parachute on his Soyuz 1 spacecraft failed to deploy."]);
+spaceEvents.push( ["April 28, 2001","American Dennis Tito became the first tourist in space after paying the Russian space program $20,000,000.					   "]);
+spaceEvents.push( ["June 2, 1966","Surveyor 1 became the first American spacecraft to land on the moon."]);
+spaceEvents.push( ["June 3, 1965","Astronaut Ed White became the first American to walk in space. "]);
+spaceEvents.push( ["June 14, 1965","The spacecraft Mariner 4 transmitted the first pictures of Mars."]);
+spaceEvents.push( ["June 16, 1962","Valentina Nikolayeva Tereshkova became the first woman in space."]);
+spaceEvents.push( ["June 18, 2009","NASA launched the Lunar Crater Observation and Sensing Satellite, also known as LCROSS. The mission is to confirm the presence or absence of ice on the moon. On November 13, 2009, NASA scientists announced the discovery of a 'significant amount' of ice in a crater near the moon’s South Pole."]);
+spaceEvents.push( ["July 1, 2004","The Cassini spacecraft sent back photographs of Saturn’s shimmering rings."]);
+spaceEvents.push( ["July 3,	2005","A NASA spacecraft collided with a comet half the size of Manhattan, creating a brilliant cosmic smashup designed to help scientists study the building blocks of life on earth."]);
+spaceEvents.push( ["July 4,	1997","The Mars Pathfinder arrived on Mars and later began transmitting images."]);
+spaceEvents.push( ["July 8,	2011","The space shuttle Atlantis became the last American space shuttle to be launched into space. Mission STS-135 and its 4-member crew brought much-needed supplies and equipment to the International Space Station (ISS)."]);
+spaceEvents.push( ["July 16, 2011","NASA's Dawn spacecraft became the first man made craft to orbit an asteroid. "]);
+spaceEvents.push( ["July 17, 1975","The American Apollo 18 and Soviet Soyuz 19 dock in the Apollo-Soyuz Test Project."]);
+spaceEvents.push( ["July 19, 1983","Sally Ride became the first American woman in space on Challenger’s second mission."]);
+spaceEvents.push( ["July 20, 1969","Neil Armstrong and 'Buzz' Aldrin became the first men on the moon."]);
+spaceEvents.push( ["July 23, 1999","Eileen Collins became the first female Shuttle Commander."]);
+spaceEvents.push( ["July 26, 2005","Space Shuttle Discovery was launched with seven astronauts aboard; this was America’s first manned space shot since the 2003 Columbia disaster."]);
+spaceEvents.push( ["July 30, 1971","The moon rover was driven on the moon for the first time. "]);
+spaceEvents.push( ["August 4, 2007","NASA launched its Phoenix Mars Lander."]);
+spaceEvents.push( ["August 6, 2012","NASAs Curiosity rover successfully landed on Mars. As large as a car, it carried an array of advanced new instruments and experiments. "]);
+spaceEvents.push( ["August 8, 2007","Space Shuttle Endeavour and a crew of seven blasted off with teacher-astronaut Barbara Morgan aboard as a crewmember. Morgan was the first teacher in space since the Challenger disaster in 1986."]);
+spaceEvents.push( ["August 10, 1990","The Magellan spacecraft began mapping the surface of Venus using radar equipment."]);
+spaceEvents.push( ["August 19, 1960","The Soviet craft Sputnik 5 was launched, carrying the dogs Strelka and Belka. They became the first living beings to survive a trip into space."]);
+spaceEvents.push( ["August 20,	2015","Nothing much happened!"]);
+spaceEvents.push( ["August 20,	2016","K S P P A O was released!"]);
+spaceEvents.push( ["August 24, 1990","The Space Shuttle Discovery deployed the Hubble Space Telescope."]);
+spaceEvents.push( ["August 25, 2003","NASA launched the largest-diameter infrared telescope ever in space, the Spitzer Space Telescope."]);
+spaceEvents.push( ["August 26, 1981","Voyager 2 reached Saturn and began transmitting images."]);
+spaceEvents.push( ["August 30, 1983","Guion Bluford became the first African-American in space."]);
+spaceEvents.push( ["August 30, 1984","The third Space Shuttle, Discovery, was launched. "]);
+spaceEvents.push( ["September 7, 2013","NASA launched the unmanned LADEE spacecraft from NASA's Wallops Flight Facility in Virginia. It was the U.S. space agency's third lunar probe in five years. "]);
+spaceEvents.push( ["September 12, 1970","The Soviet craft Luna 16 was launched and became the first automatic spacecraft to return soil samples of the moon. "]);
+spaceEvents.push( ["September 12, 1992","Mae Jemison became the first African-American woman in space."]);
+spaceEvents.push( ["September 15, 1968","The Soviet spacecraft Zond 5 was launched and later became the first spacecraft to orbit the moon and return to Earth. "]);
+spaceEvents.push( ["September 21, 2003","NASA’s Galileo mission ended a 14-year exploration of the solar system’s largest planet and its moons with the spacecraft crashing by design into Jupiter at 108,000 mph."]);
+spaceEvents.push( ["October 3, 1985","Atlantis, the fourth Space Shuttle, was launched."]);
+spaceEvents.push( ["October 4, 1957","The Soviet Union launched the first satellite, Sputnik, into space. "]);
+spaceEvents.push( ["October 10, 2010","Virgin Galactic, a private company, announced the successful first manned glide flight of the VSS Enterprise. This vehicle is a suborbital plane designed to take private citizens on suborbital space flights. "]);
+spaceEvents.push( ["October 11,	2010","President Barack Obama signed legislation focusing NASA’s efforts on exploring Mars and the asteroids. "]);
+spaceEvents.push( ["October 18,	1967","A descent capsule from the Soviet probe Venera 4 collected data about the atmosphere of Venus."]);
+spaceEvents.push( ["October 29,	1998","John Glenn became the oldest man in space."]);
+spaceEvents.push( ["November 3,	1957","The Soviet spacecraft Sputnik 2 was launched with a dog named Laika on board. Laika did not survive the voyage."]);
+spaceEvents.push( ["November 13, 1971","The Mariner 9 probe became the first craft to orbit another world - Mars."]);
+spaceEvents.push( ["November 13, 1980","Voyager 1 reached Saturn and began transmitting images."]);
+spaceEvents.push( ["November 17, 1970","The Soviet automatic robot Lunokhod 1 landed on the moon with Luna 17. "]);
+spaceEvents.push( ["November 26, 2011","NASA launched Curiosity, the biggest, best equipped robot ever sent to explore another planet. It will reach Mars in 2012. "]);
+spaceEvents.push( ["December 8,	2010","A private company named SpaceX launched a spacecraft into orbit and returned it to earth safely. It was the first non-government organization to accomplish this."]);
+spaceEvents.push( ["December 11, 1972","Eugene Cernan and Harrison 'Jack' Schmitt became the last men to walk on the moon."]);
+spaceEvents.push( ["December 15, 1970","The Soviet Venera 7 became the first probe to land on Venus."]);
+spaceEvents.push( ["December 21,1968","Apollo 8 was launched, and later her crewmembers became the first men to orbit the moon."]);
+spaceEvents.push( ["December24,	2013","NASA astronauts wrapped up successful repairs at the International Space Station after a rare Christmas Eve spacewalk to fix an equipment cooling system. "]);
+
+function getEvent(){
+	var objDate = new Date(),
+	locale = "en-us",
+	month = objDate.toLocaleString(locale, { month: "long" });
+	day   = objDate.getDate();
+	today = month+" "+day;
+	//console.log("today: "+today);
+	themessage = "";
+	for(var i=0;i<spaceEvents.length;i++){	
+		if (spaceEvents[i][0].includes(today)){
+			//console.log("found: "+spaceEvents[i][0]);
+			if (themessage.length>0){
+				themessage = themessage + " also on "+spaceEvents[i][0]+", "+spaceEvents[i][1];	
+			} else {
+				themessage = "Today in history, on "+spaceEvents[i][0]+", "+spaceEvents[i][1];	
+			}
+			
+			
+		}
+	}
+	return (themessage);
+}
